5991f278672651fd1c486963aa9a8851cc061fd9
alsafau10 <sayaituburiq3000@gmail.com>
2024-03-22 19:32:38 +0700

    menambahkan beberapa file

--- modul-ajax/script.js
+++ modul-ajax/script.js
@@ -1,34 +1,33 @@
 $(document).ready(function() {
-    $("#load-data").click(function() {
-      // Clear existing table data
-      $('#data-body').empty();
-  
-      $.ajax({
-        type: "GET",
-        url: "data.xml",
-        dataType: "xml",
-        success: function(xml) {
-          var tbody = $('#data-body');
-          $(xml).find('table_data').find('item').each(function() {
-            var title = $(this).find('title').text();
-            var description = $(this).find('description').text();
-            var row = '<tr><td>' + title + '</td><td>' + description + '</td></tr>';
-            tbody.append(row);
-          });
-          
-          var hiddenText = $(xml).find('hidden_text').text();
-          var hiddenTextElement = $("#hidden-text");
-          
-          if (hiddenTextElement.is(":visible")) {
-            hiddenTextElement.hide();
-          } else {
-            hiddenTextElement.text(hiddenText).show();
-          }
-        },
-        error: function(xhr, status, error) {
-          console.error("Error:", status, error);
+  $("#load-data").click(function() {
+    // Clear existing table data
+    $('#data-body').empty();
+
+    $.ajax({
+      type: "GET",
+      url: "data.xml",
+      dataType: "xml",
+      success: function(xml) {
+        var tbody = $('#data-body');
+        $(xml).find('table_data').find('item').each(function() {
+          var title = $(this).find('title').text();
+          var description = $(this).find('description').text();
+          var row = '<tr><td>' + title + '</td><td>' + description + '</td></tr>';
+          tbody.append(row);
+        });
+        
+        var hiddenText = $(xml).find('hidden_text').text();
+        var hiddenTextElement = $("#hidden-text");
+        
+        if (hiddenTextElement.is(":visible")) {
+          hiddenTextElement.hide();
+        } else {
+          hiddenTextElement.text(hiddenText).show();
         }
-      });
+      },
+      error: function(xhr, status, error) {
+        console.error("Error:", status, error);
+      }
     });
   });
-  
+});
